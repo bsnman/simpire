4977786e013230392fe662f176ad48720c8591c2
Radih Jr Paraguas <radihjrprgs@gmail.com>
2026-02-21 16:21:54 +1000

    Add SCA, eslint (tslint) and prettier

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginVue from "eslint-plugin-vue";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "coverage/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginVue.configs["flat/recommended"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  eslintConfigPrettier,
+];
