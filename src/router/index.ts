4350ba781dc8195b72a4698ea84af5599dfb8652
Radih Jr Paraguas <radihjrprgs@gmail.com>
2026-02-22 15:44:40 +1000

    Add router

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeView from '../views/HomeView.vue';
+import GameView from '../views/GameView.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/game/:id',
+      name: 'game',
+      component: GameView,
+      props: true,
+    },
+  ],
+});
+
+export default router;
